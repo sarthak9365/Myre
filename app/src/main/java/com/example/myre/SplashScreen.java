eda602b19482ce52dc47b6092700f844aa9abf06
Naiket08 <60822686+Naiket08@users.noreply.github.com>
2020-03-04 13:15:16 +0530

    Changes in Splashscreen

--- app/src/main/java/com/example/myre/SplashScreen.java
+++ app/src/main/java/com/example/myre/SplashScreen.java
@@ -2,7 +2,9 @@ package com.example.myre;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
@@ -21,5 +23,14 @@ public class SplashScreen extends AppCompatActivity {
         animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fadi_in);
 
         myre_title.startAnimation(animation);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this,LoginScreen.class);
+                startActivity(intent);
+            }
+        },3000);
     }
 }
