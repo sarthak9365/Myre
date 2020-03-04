a910cb4c5f4767e386b423f03b4fdbbc6f16f8db
sarthak9365 <60870823+sarthak9365@users.noreply.github.com>
2020-03-04 13:04:23 +0530

    different

--- app/src/main/java/com/example/myre/LoginScreen.java
+++ app/src/main/java/com/example/myre/LoginScreen.java
@@ -3,12 +3,26 @@ package com.example.myre;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginScreen extends AppCompatActivity {
-
+    EditText email,password;
+    Button login;
+    TextView newuser1,signupuser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+        email=(EditText)findViewById(R.id.emailid);
+        password=(EditText)findViewById(R.id.password);
+        login=(Button)findViewById(R.id.login);
+        newuser1=(TextView)findViewById(R.id.newuser);
+        signupuser=(TextView)findViewById(R.id.signup);
     }
+
+        signupuser.On
+
 }
