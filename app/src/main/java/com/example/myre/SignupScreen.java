dae51720ee91eae2aba6c345fb4bd8d5f01e309e
sarthak9365 <60870823+sarthak9365@users.noreply.github.com>
2020-03-04 13:33:49 +0530

    pages are created successfully

--- app/src/main/java/com/example/myre/SignupScreen.java
+++ app/src/main/java/com/example/myre/SignupScreen.java
@@ -3,12 +3,22 @@ package com.example.myre;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignupScreen extends AppCompatActivity {
-
+    EditText fname,lname,email,pwd,cpwd;
+    Button signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_screen);
+        fname= (EditText) findViewById(R.id.firstname);
+        lname=(EditText) findViewById(R.id.lastname);
+        email=(EditText) findViewById(R.id.emailId_1);
+        pwd=(EditText) findViewById(R.id.password);
+        cpwd=(EditText) findViewById(R.id.cpassword);
+        signup=(Button)findViewById(R.id.signup1);
+
     }
 }
