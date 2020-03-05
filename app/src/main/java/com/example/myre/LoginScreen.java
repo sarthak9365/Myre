4ccc632ee06af381c8b3c81f929d907bf98152ac
Naiket08 <60822686+Naiket08@users.noreply.github.com>
2020-03-05 19:24:12 +0530

    Done

--- app/src/main/java/com/example/myre/LoginScreen.java
+++ app/src/main/java/com/example/myre/LoginScreen.java
@@ -12,8 +12,9 @@ import android.widget.Toast;
 
 public class LoginScreen extends AppCompatActivity {
     EditText email,password;
-    String str1,str2;
     Button login;
+    String str1 = "admin";
+    String str2 = "admin";
     TextView newuser1,signupuser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,31 +25,29 @@ public class LoginScreen extends AppCompatActivity {
         login=(Button)findViewById(R.id.login);
         newuser1=(TextView)findViewById(R.id.newuser);
         signupuser=(TextView)findViewById(R.id.signup);
-        signupuser.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LoginScreen.this,SignupScreen.class);
-                startActivity(intent);
-            }
-        });
 
-        //str1=email.getText().toString();
-        //str2=password.getText().toString();
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(email.getText().toString()=="admin"){
-                    if(str2=="12345678"){
-                        Toast.makeText(LoginScreen.this,"Login Successfully",Toast.LENGTH_LONG).show();
+                if(email.getText().toString().equals(str1)){
+                    if(password.getText().toString().equals(str2)){
+                        Toast.makeText(LoginScreen.this, "Login Successful", Toast.LENGTH_SHORT).show();
                     }
                     else{
                         Toast.makeText(LoginScreen.this, "Wrong Password", Toast.LENGTH_SHORT).show();
                     }
                 }
                 else{
-                    Toast.makeText(LoginScreen.this, ""+email.getText().toString(), Toast.LENGTH_SHORT).show();
                     Toast.makeText(LoginScreen.this, "Wrong EmailID", Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+
+        signupuser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginScreen.this,SignupScreen.class);
+                startActivity(intent);
             }
         });
     }
